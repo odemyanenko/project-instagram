2f402016c5cb22ca8949091c306de78cf97de11e
marinasavluchinska <anauk2018@gmail.com>
2019-07-07 12:28:27 +0300

    icon as element

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,13 @@ import './App.css';
 
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {fab} from '@fortawesome/free-brands-svg-icons';
-import {fas} from '@fortawesome/free-solid-svg-icons';
+import {fas,faInfoCircle} from '@fortawesome/free-solid-svg-icons';
 import {far} from '@fortawesome/free-regular-svg-icons';
 import Header from "../src/components/header";
 
 import HomePage from "../src/components/pages/homePage";
 import PersonalPage from "./components/pages/personalPage/personalPage";
-library.add(fab, fas, far);
+library.add(fab, fas, far, faInfoCircle);
 
 
 function App() {
--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './header.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faInfoCircle} from "@fortawesome/free-solid-svg-icons/faInfoCircle";
+import {faHeart} from "@fortawesome/free-solid-svg-icons/faHeart";
 
-const Icon = () => {
+const Icon = (props) => {
     return (
-        <a className="text-dark mx-2" href=""><FontAwesomeIcon icon={['fab', 'instagram']} size="1x"/></a>
+        <a className="text-dark mx-2" href=""><FontAwesomeIcon icon={props.icon} size="1x"/></a>
     )
 }
 const Header =()=>{
     return (
         <div className="header-container">
-        <Icon/>
+        <Icon icon={faInfoCircle}/>
         <h1>Instagram</h1>
-            <Icon/>
-            <Icon/>
-            <Icon/>
+            <Icon icon={faHeart}/>
+            <Icon icon="adjust"/>
+            <Icon icon="circle"/>
             </div>
     )
 }
