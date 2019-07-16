461b85fe8b2ea73a9f6bbfeecfb9e312aeafa9d6
Vova Shkil <vshkil@gmail.com>
2019-07-16 04:38:31 -0400

    userPage page takes users from external API

--- client/src/components/users/Users.jsx
+++ client/src/components/users/Users.jsx
@@ -1,36 +1,106 @@
 import React from 'react';
 import classes from './Users.module.scss';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user-icon-small.png';
 
 let Users = (props) => {
-    console.log(props.users);
+    if (props.users.length === 0) {
+
+        // props.setUsers([
+        //     {
+        //         id: 1,
+        //         photoUrl: 'http://tomorrowslawyer.org/wp-content/uploads/2017/01/dmytro-kukhnyuk_800x800_acf_cropped.jpg',
+        //         followed: false,
+        //         firstName: "Dmytro",
+        //         lastName: "Sydoriv",
+        //         status: "I'm a boss",
+        //         location: {city: "Kyiv", country: "Ukraine"}
+        //     },
+        //     {
+        //         id: 2,
+        //         photoUrl: 'http://politika-v-rashke.ru/wp-content/uploads/2018/09/vasya-voskres.jpg',
+        //         followed: false,
+        //         firstName: "Vasia",
+        //         lastName: "Pupkin",
+        //         status: "I'm available",
+        //         location: {city: "Dnipro", country: "Ukraine"}
+        //     },
+        //     {
+        //         id: 3,
+        //         photoUrl: 'https://upload.wikimedia.org/wikipedia/uk/6/68/Poddubny.jpg',
+        //         followed: true,
+        //         firstName: "Ivan",
+        //         lastName: "Ivanov",
+        //         status: "Hey, hello",
+        //         location: {city: "Toronto", country: "Canada"}
+        //     },
+        //     {
+        //         id: 4,
+        //         photoUrl: 'https://upload.wikimedia.org/wikipedia/uk/6/68/Poddubny.jpg',
+        //         followed: true,
+        //         firstName: "Petro",
+        //         lastName: "Petrov",
+        //         status: "How are you?",
+        //         location: {city: "Lviv", country: "Ukraine"}
+        //     },
+        //     {
+        //         id: 5,
+        //         photoUrl: 'https://upload.wikimedia.org/wikipedia/uk/6/68/Poddubny.jpg',
+        //         followed: false,
+        //         firstName: "Sashko",
+        //         lastName: "Shushko",
+        //         status: "What's your mood?",
+        //         location: {city: "Warsaw", country: "Poland"}
+        //     },
+        //     {
+        //         id: 6,
+        //         photoUrl: 'https://upload.wikimedia.org/wikipedia/uk/6/68/Poddubny.jpg',
+        //         followed: true,
+        //         firstName: "Paraska",
+        //         lastName: "Paraskiva",
+        //         status: "I'm pretty",
+        //         location: {city: "Boston", country: "US"}
+        //     },
+        // ]);
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                props.setUsers(response.data.items);
+            });
+    }
+
     return (
         <div>
             {
-                props.users.map(user => <div key={user.id}>
-                <span>
-                    <div>
-                        <img src={user.photoUrl} className={classes.userPhoto} alt=""/>
-                </div>
-                <div>
-                    {
-                        user.followed
-                            ? <button onClick={() => (props.unFollow(user.id))}>Follow</button>
-                            : <button onClick={() => (props.follow(user.id))}>Unfollow</button>
-                    }
+                props.users.map(user =>
+                    <div key={user.id}>
+                         <span>
+                            <div>
+                                {/*<img src={user.photoUrl} className={classes.userPhoto} alt=""/>*/}
+                                <img src={ user.photos.small != null ? user.photos.small : userPhoto } className={classes.userPhoto} alt=""/>
+                            </div>
+                            <div>
+                                {
+                                    user.followed
+                                        ? <button onClick={() => (props.unFollow(user.id))}>Follow</button>
+                                        : <button onClick={() => (props.follow(user.id))}>Unfollow</button>
+                                }
 
-                </div>
-                </span>
-                    <span>
-                <span>
-                    <div>{user.firstName} {user.lastName}</div>
-                    <div>{user.status}</div>
-                </span>
-                    <span>
-                    <div>{user.location.country}</div>
-                    <div>{user.location.city}</div>
-                </span>
-                </span>
-                </div>)
+                            </div>
+                        </span>
+
+                        <span>
+                             <span>
+                                {/*<div>{user.firstName} {user.lastName}</div>*/}
+                                <div>{user.name}</div>
+                                 <div>{user.status}</div>
+                             </span>
+                             <span>
+                                <div>{"user.location.country"}</div>
+                                <div>{"user.location.city"}</div>
+                            </span>
+                        </span>
+                    </div>)
             }
 
         </div>);
--- client/src/reducers/users/users-reducer.js
+++ client/src/reducers/users/users-reducer.js
@@ -4,12 +4,12 @@ const SET_USERS = "SET_USERS";
 
 let initialState = {
     users: [
-        {id: 1, photoUrl: 'http://tomorrowslawyer.org/wp-content/uploads/2017/01/dmytro-kukhnyuk_800x800_acf_cropped.jpg', followed: false, firstName: "Dmytro", lastName: "Sydoriv", status: "I'm a boss", location: { city: "Kyiv", country: "Ukraine"}},
-        {id: 2, photoUrl: 'http://politika-v-rashke.ru/wp-content/uploads/2018/09/vasya-voskres.jpg', followed: false, firstName: "Vasia", lastName: "Pupkin", status: "I'm available", location: { city: "Dnipro", country: "Ukraine"}},
-        {id: 3, photoUrl: 'https://upload.wikimedia.org/wikipedia/uk/6/68/Poddubny.jpg', followed: true, firstName: "Ivan", lastName: "Ivanov", status: "Hey, hello", location: { city: "Toronto", country: "Canada"}},
-        {id: 4, photoUrl: 'https://upload.wikimedia.org/wikipedia/uk/6/68/Poddubny.jpg', followed: true, firstName: "Petro", lastName: "Petrov", status: "How are you?", location: { city: "Lviv", country: "Ukraine"}},
-        {id: 5, photoUrl: 'https://upload.wikimedia.org/wikipedia/uk/6/68/Poddubny.jpg', followed: false,firstName: "Sashko", lastName: "Shushko", status: "What's your mood?", location: { city: "Warsaw", country: "Poland"}},
-        {id: 6, photoUrl: 'https://upload.wikimedia.org/wikipedia/uk/6/68/Poddubny.jpg', followed: true,firstName: "Paraska", lastName: "Paraskiva", status: "I'm pretty", location: { city: "Boston", country: "US"}},
+        // {id: 1, photoUrl: 'http://tomorrowslawyer.org/wp-content/uploads/2017/01/dmytro-kukhnyuk_800x800_acf_cropped.jpg', followed: false, firstName: "Dmytro", lastName: "Sydoriv", status: "I'm a boss", location: { city: "Kyiv", country: "Ukraine"}},
+        // {id: 2, photoUrl: 'http://politika-v-rashke.ru/wp-content/uploads/2018/09/vasya-voskres.jpg', followed: false, firstName: "Vasia", lastName: "Pupkin", status: "I'm available", location: { city: "Dnipro", country: "Ukraine"}},
+        // {id: 3, photoUrl: 'https://upload.wikimedia.org/wikipedia/uk/6/68/Poddubny.jpg', followed: true, firstName: "Ivan", lastName: "Ivanov", status: "Hey, hello", location: { city: "Toronto", country: "Canada"}},
+        // {id: 4, photoUrl: 'https://upload.wikimedia.org/wikipedia/uk/6/68/Poddubny.jpg', followed: true, firstName: "Petro", lastName: "Petrov", status: "How are you?", location: { city: "Lviv", country: "Ukraine"}},
+        // {id: 5, photoUrl: 'https://upload.wikimedia.org/wikipedia/uk/6/68/Poddubny.jpg', followed: false,firstName: "Sashko", lastName: "Shushko", status: "What's your mood?", location: { city: "Warsaw", country: "Poland"}},
+        // {id: 6, photoUrl: 'https://upload.wikimedia.org/wikipedia/uk/6/68/Poddubny.jpg', followed: true,firstName: "Paraska", lastName: "Paraskiva", status: "I'm pretty", location: { city: "Boston", country: "US"}},
     ]
 };
 
